7cdc02c6832f9846d0eadaafb663dae6a2b7068d
kantiya13 <kantiyachiablaem13@gmail.com>
2019-05-05 00:07:20 +0700

    update

--- db_connection.php
+++ db_connection.php
@@ -1,16 +1,17 @@
 <?php
 		//For Server
-		$servername = "localhost";
-		$username = "id9352898_phonicapp";
-		//$username = "id1057438_phonic_app";
-		$password = "123456";
-		$dbname = "id9352898_phonicapp";
+		// $servername = "localhost";
+		// $username = "id9352898_phonicapp";
+		// //$username = "id1057438_phonic_app";
+		// $password = "123456";
+		// $dbname = "id9352898_phonicapp";
 		//$dbname = "id1057438_phonic_app";
 
 		//For Local
-		//$servername = "localhost";
-		//$username = "root";
-		//$password = "12345678";
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "id9352898_phonicapp";
 		//$dbname = "ncst";
 
 		// Create connection
