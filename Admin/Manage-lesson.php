2858988b8ffe7eeaa2a6aa6c25fdf36de2f75488
kantiya13 <kantiyachiablaem13@gmail.com>
2019-05-12 21:18:50 +0700

    update Manage-lesson แสดงข้อมูลในตาราง

--- Admin/Manage-lesson.php
+++ Admin/Manage-lesson.php
@@ -1,8 +1,13 @@
+<?php
+include '../db_connection.php';
+$sql = "SELECT * FROM lesson_detail ORDER BY `level` ASC";
+$query = mysqli_query($conn, $sql);
+?>
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
 
 <head>
-<link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css" rel="stylesheet">
+    <link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css" rel="stylesheet">
     <title>Administrator Phonic App by Aj.Aum</title>
 </head>
 
@@ -17,13 +22,13 @@
     <!-- Main wrapper - style you can find in pages.scss -->
     <div id="main-wrapper">
         <!-- Topbar header - style you can find in pages.scss -->
-        <?php include 'template/menu.php';?>
+        <?php include 'template/menu.php'; ?>
         <!-- End Topbar header -->
 
         <!-- Page wrapper  -->
         <div class="page-wrapper">
             <!-- Bread crumb and right sidebar toggle -->
-             <div class="page-breadcrumb">
+            <div class="page-breadcrumb">
                 <div class="row">
                     <div class="col-12 d-flex no-block align-items-center">
                         <h4 class="page-title">จัดการข้อมูลบทเรียนและคำศัพท์</h4>
@@ -44,57 +49,71 @@
                 <!-- แสดงตารางข้อมูลผู้ใช้งาน -->
                 <div class="card">
                     <!-- ปุ่มเพิ่มข้อมุล -->
-                        <div class="card-body">
-                            <a href="form-add-lesson.php" class="btn btn-success btn-sm"><i class="fas fa-plus-circle"></i> เพิ่มข้อมูลบทเรียน</a>
-                        </div>
-                     <!-- ปุ่มเพิ่มข้อมุล -->
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table id="zero_config" class="table table-bordered table-sm">
-                                        <thead class="table-secondary">
-                                            <tr >
-                                                <th width = "15px">ลำดับ</th>
-                                                <th>Level</th>
-                                                <th>ชื่อบทเรียน</th>
-                                                <th>คำอธิบายบทเรียน</th>
-                                                <th>รูปบทเรียน</th>
-                                                <th width ="50px">ตัวเลือก</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>1</td>
-                                                <td>Lesson 1</td>
-                                                <td>Letter Sounds</td>
-                                                <td><img src="" alt="image-leeson" height="50" width="50"></td>
-                                                <td>
-                                                    <a href="#" style="color: gray;" title="view"><i class="fas fa-search"></i></a>
-                                                    <a href="form-edit-lesson.php" style="color: green;" title="แก้ไขข้อมูล"><i class="far fa-edit"></i></a>
-                                                    <a href="" style="color: red;" title="ลบข้อมูล"><i class="fas fa-times-circle"></i></a>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
+                    <div class="card-body">
+                        <a href="form-add-lesson.php" class="btn btn-success btn-sm"><i class="fas fa-plus-circle"></i> เพิ่มข้อมูลบทเรียน</a>
+                    </div>
+                    <!-- ปุ่มเพิ่มข้อมุล -->
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="zero_config" class="table table-bordered table-sm table-hover">
+                                <thead class="table-secondary">
+                                    <tr>
+                                        <th width="15px">ลำดับ</th>
+                                        <th>Level</th>
+                                        <th>ชื่อบทเรียน</th>
+                                        <th>คำอธิบายบทเรียน</th>
+                                        <th>รูปบทเรียน</th>
+                                        <th width="50px">ตัวเลือก</th>
+                                    </tr>
+                                </thead>
 
-                            </div>
+                                <tbody>
+                                    <?php
+                                    $i = 1;
+                                    while ($result = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+                                        ?>
+                                        <tr>
+                                            <td><?= number_format($i); ?></td>
+                                            <td><?php echo $result["level"]; ?></td>
+                                            <td><?php echo $result["lesson_name"]; ?></td>
+                                            <td><?php echo $result["lesson_desc"]; ?></td>
+                                            <td><img src="<?php echo $result["big_image"]; ?>"></td>
+
+                                            <td width="65px;" align="center">
+                                                <a href="#" style="color: gray;" title="view" class="view_data" id="<?php echo $result["user_id"]; ?>"><i class="fas fa-search"></i></a>
+
+                                                <a href="JavaScript:if(confirm('คุณต้องการลบข้อมูลใช่หรือไม่ ?')==true){window.location='script/deluser_script.php?user_id=<?php echo $result["user_id"]; ?>';}" style="color: red;" title="ลบข้อมูล"><i class="fas fa-times-circle"></i></a>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                        $i++;
+                                    }
+                                    ?>
+                                </tbody>
+
+                            </table>
+                            <?php
+                            mysqli_close($conn);
+                            ?>
                         </div>
+
+                    </div>
+                </div>
                 <!-- Sales chart -->
             </div>
             <!-- End Container fluid  -->
             <!-- footer -->
-            <?php include 'template/footer.php';?>
+            <?php include 'template/footer.php'; ?>
             <!-- End footer -->
         </div>
         <!-- End Page wrapper  -->
     </div>
-    
+
     <!-- End Wrapper -->
     <!-- All Jquery -->
     <script src="assets/extra-libs/DataTables/datatables.min.js"></script>
     <script>
-         /* Basic Table */
+        /* Basic Table */
         $('#zero_config').DataTable();
     </script>
 
