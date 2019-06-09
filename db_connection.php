7491d8ca8446410521b5859862b2b328c2621e45
kantiya13 <kantiyachiablaem13@gmail.com>
2019-06-09 21:17:40 +0700

    update

--- db_connection.php
+++ db_connection.php
@@ -1,17 +1,17 @@
 <?php
 		//For Server
-		$servername = "localhost";
-		$username = "id9352898_phonicapp";
-		//$username = "id1057438_phonic_app";
-		$password = "123456";
-		$dbname = "id9352898_phonicapp";
-		//$dbname = "id1057438_phonic_app";
+		// $servername = "localhost";
+		// $username = "id9352898_phonicapp";
+		// //$username = "id1057438_phonic_app";
+		// $password = "123456";
+		// $dbname = "id9352898_phonicapp";
+		// //$dbname = "id1057438_phonic_app";
 
 		//For Local
-		//$servername = "localhost";
-		//$username = "root";
-		//$password = "12345678";
-		//$dbname = "ncst";
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "id9352898_phonicapp";
 
 		// Create connection
 		$conn = new mysqli($servername, $username, $password, $dbname);
